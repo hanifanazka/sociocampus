ab9e7b28ae3d98f744eb8d634bd3c9a48696348a
Hanifan Azka <hanifan.azka@gmail.com>
2021-08-23 13:13:29 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Sociocampus",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+};
